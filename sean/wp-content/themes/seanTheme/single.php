56693373b0b3944d4da5f6bb76f4178ee09f6af4
matt <matty_ander@hotmail.com>
2017-02-16 19:16:19 -0500

    fixing an error

--- sean/wp-content/themes/seanTheme/single.php
+++ sean/wp-content/themes/seanTheme/single.php
@@ -15,23 +15,7 @@
         <div class="col-md-8">
             <?php if ( have_posts() ) : ?>
                 <?php while ( have_posts() ) : the_post(); ?>
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-                    <h1><?php the_title(); ?></h1>
-                    <p><?php the_date(); ?></p>
-                    <?php the_content(); ?>
-                    
-                    <?php
-                        wp_link_pages( array(
-                            'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages', 'customtheme') . '</span>',
-                            'after'       => '</div>',
-                            'link_before' => '<span',
-                            'link_after'  => '</span>',
-                            'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'customtheme' ) . '</span>%',
-                            'separator'   => '<span class="screen-reader-text">, </span>'
-                        ));
-                    ?>
-                    
-                </div>
+                <?php get_template_part('template-parts/content','single'); ?>
 
                 <?php endwhile; ?>
             <?php endif; ?>
