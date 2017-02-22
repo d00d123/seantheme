b0f62ac24af02ebaba3991ef17c18085373b5167
matt <matty_ander@hotmail.com>
2017-02-22 18:07:43 -0500

    setting up basic nav

--- sean/wp-content/themes/seanTheme/header.php
+++ sean/wp-content/themes/seanTheme/header.php
@@ -8,33 +8,32 @@
 
 ?>
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+    <!DOCTYPE html>
+    <html <?php language_attributes(); ?>>
+
     <head>
         <meta name="description" content="My first custom WordPress theme.">
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?> <!--This is the hook so you can drop in plugins-->
+            <?php wp_head(); ?>
+                <!--This is the hook so you can drop in plugins-->
     </head>
-    
+
     <?php echo '<body class="'.join(' ', get_body_class()).'">'.PHP_EOL; ?>
+
+
         <!--Navigation is first-->
-        <nav class="navbar navbar-default navbar-fixed-top gradient-bg main-theme-nav">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="https://wordpress-testing-d00d123.c9users.io/" class="navbar-brand">Brand</a>
-                </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <?php 
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <!--input WordPress nav-->
+                    <?php 
                             wp_nav_menu( array( 
                                 'theme_location' => 'primary',
                                 'menu_class'     => 'primary-menu',
@@ -42,7 +41,8 @@
                                 'items_wrap'     => '%3$s'
                             ) );
                         ?>
-                    </ul>
-                </div>
+                </ul>
             </div>
+            <a class="navbar-brand" href="//mattpr.info/sean">Sean Greenburg</a>
+
         </nav>
