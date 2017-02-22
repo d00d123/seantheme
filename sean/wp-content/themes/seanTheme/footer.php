8464a67d6e316caff52709c1df2aef1f949f1673
matt <matty_ander@hotmail.com>
2017-02-22 18:44:08 -0500

    did nothign

--- sean/wp-content/themes/seanTheme/footer.php
+++ sean/wp-content/themes/seanTheme/footer.php
@@ -14,7 +14,7 @@
             //this script hacks the bootstrap 4 classes onto the wordpress nav items
             
             $('li.menu-item').addClass('nav-item');
-            $('li.menu-item a').addClass('nav-link');
+            $('li.menu-item > a').addClass('nav-link');
             
         </script>
 
