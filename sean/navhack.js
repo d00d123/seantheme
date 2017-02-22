9750e856691831f5b2c0fcf3a52d812b0c3094a7
matt <matty_ander@hotmail.com>
2017-02-22 18:58:18 -0500

    still troubleshooting

---
+++ sean/navhack.js
@@ -0,0 +1,6 @@
+//this script hacks the bootstrap 4 classes onto the wordpress nav items
+$(document).ready(function () {
+    $('li.menu-item').addClass('nav-item');
+    $('li.menu-item > a').addClass('nav-link');
+    $('li.current-menu-item').addClass('active');
+});
--- sean/wp-content/themes/seanTheme/footer.php
+++ sean/wp-content/themes/seanTheme/footer.php
@@ -9,15 +9,6 @@
 
     <?php wp_footer(); ?>
 
-        <!--scripts-->
-        <script>
-            //this script hacks the bootstrap 4 classes onto the wordpress nav items
-            $(document).ready(function () {
-                //$('li.menu-item').addClass('nav-item');
-                $('li.menu-item > a').addClass('nav-link');
-                //$('li.current-menu-item').addClass('active');
-            });
-        </script>
 
         </body>
 
